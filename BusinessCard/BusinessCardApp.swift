846856f526ad851b835b2a2c5de0d5c0198eb3fd
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-11-25 18:13:59 -0300

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by kenjimaeda on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
